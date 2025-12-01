4371161850b7e7b39eb9b1c0427a6f77cf10cf7e
Art3mis <ricatom2003@gmail.com>
2025-12-01 02:52:02 -0500

    day01 part 2 - Result for part 2 is : 6358 Excecuted part 2 in 0.0009917900006257696 seconds

--- src/day01.py
+++ src/day01.py
@@ -44,9 +44,57 @@ def part1(moves):
 import time
 
 start = time.perf_counter()  # for information purpose
-
 print("Result for part 1 is : {}".format(part1(moves)))
-
 end = time.perf_counter()
-
 print("Excecuted part 1 in {} seconds".format(end - start))
+
+# Result for part 2 is : 6358
+# Excecuted part 1 in 0.00024656899950059596 seconds
+
+
+# --- Part 2
+#
+def step2(dial, move):
+    value = move[1]
+    if move[0] == "R":
+        t = (numbers - dial) % numbers  # numbers of digit until 0
+        totalValue = dial + value
+    else:
+        t = dial % numbers
+        totalValue = dial - value
+
+    dial = totalValue % numbers  # remainder of the euclidian division
+
+    if value < t:
+        clicks = 0
+    else:
+        clicks = 1 + (value - t) // numbers  # quotient of the euclidian division
+
+    if dial == 0 and clicks > 0:
+        clicks -= 1
+
+    return dial, clicks
+
+
+def part2(moves):
+    dial = base
+    clicksCount = 0
+    for move in moves:
+        dial, clicks = step2(dial, move)
+        clicksCount += clicks
+
+        # to help comprehension
+        # print("Current move : {}, Dial : {}, Count : {}".format(move, dial, clicksCount))
+
+    return clicksCount
+
+
+# Part2 - Solve
+
+start = time.perf_counter()  # for information purpose
+print("Result for part 2 is : {}".format(part2(moves)))
+end = time.perf_counter()
+print("Excecuted part 2 in {} seconds".format(end - start))
+
+# Result for part 2 is : 6358
+# Excecuted part 2 in 0.000457536001704284 seconds
